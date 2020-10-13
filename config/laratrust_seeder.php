dc32cae7fc8fb57904160cbd3f0873cbfa4e1960
Maryann Gitonga <34104277+MaryannGitonga@users.noreply.github.com>
2020-10-13 20:24:20 +0300

    Add Laratrust Admin Panel

---
+++ config/laratrust_seeder.php
@@ -0,0 +1,37 @@
+<?php
+
+return [
+    /**
+     * Control if the seeder should create a user per role while seeding the data.
+     */
+    'create_users' => false,
+
+    /**
+     * Control if all the laratrust tables should be truncated before running the seeder.
+     */
+    'truncate_tables' => true,
+
+    'roles_structure' => [
+        'administrator' => [
+            'users' => 'c,r,u,d',
+            'profile' => 'r,u',
+            'posts' => 'r,d',
+        ],
+        'lecturer' => [
+            'profile' => 'c,r,u,d',
+            'posts' => 'c,r,u,d,cl',
+        ],
+        'student' => [
+            'profile' => 'c,r,u,d',
+            'posts' => 'c,r,u,d',
+        ]
+    ],
+
+    'permissions_map' => [
+        'c' => 'create',
+        'r' => 'read',
+        'u' => 'update',
+        'd' => 'delete',
+        'cl' => 'close',
+    ]
+];
