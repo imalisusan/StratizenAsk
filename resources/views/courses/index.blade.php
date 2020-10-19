5be01e878386ba5ee12329d8bfaa1e9319367091
Maryann Gitonga <34104277+MaryannGitonga@users.noreply.github.com>
2020-10-19 17:49:45 +0300

    Add restriction to authorised users on Course and User Controllers

--- resources/views/courses/index.blade.php
+++ resources/views/courses/index.blade.php
@@ -4,7 +4,7 @@
     <div class="row">
         <div class="col-lg-12 margin-tb">
             <div class="pull-left">
-                <h2>Vumbua</h2>
+                <h2>Courses</h2>
             </div>
             <div class="pull-right">
                 <a class="btn btn-success" href="{{ route('courses.create') }}"> Create new Course</a>
