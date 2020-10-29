c738c921ab5c1f41327df286ed954008eb661f5c
Maryann Gitonga <34104277+MaryannGitonga@users.noreply.github.com>
2020-10-29 21:53:45 +0300

    Add changes to users index blade template in the admin panel

--- resources/views/courses/index.blade.php
+++ resources/views/courses/index.blade.php
@@ -2,7 +2,12 @@
 @section('title', 'Courses')
 @section('content')
 
-    <div class="flex flex-col">
+<div class="flex flex-col">
+    @if ($message = Session::get('success'))
+            <div class="alert alert-success">
+                <p>{{ $message }}</p>
+            </div>
+    @endif
     <a
       href="{{ route('courses.create') }}"
       class="self-end bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
@@ -15,7 +20,7 @@
           <thead>
             <tr>
               <th class="th">Id</th>
-              <th class="th">Name</th>
+              <th class="th">Course Name</th>
               <th class="th"></th>
             </tr>
           </thead>
@@ -29,7 +34,8 @@
                 {{$course->name}}
               </td>
               <td class="flex justify-end px-6 py-4 whitespace-no-wrap text-right border-b border-gray-200 text-sm leading-5 font-medium">
-                <a class="text-blue-600 hover:text-blue-900" href="{{ route('courses.edit',$course->id) }}">Edit</a>
+                <a class="text-blue-600 hover:text-blue-900" href="{{ route('courses.show',$course->id) }}">Show</a>
+                <a class="text-blue-600 hover:text-blue-900 ml-4" href="{{ route('courses.edit',$course->id) }}">Edit</a>
 
                 <form action="{{ route('courses.destroy',$course->id) }}" method="POST" onsubmit="return confirm('Are you sure you want to delete the course?');">
                     @csrf
@@ -43,6 +49,6 @@
         </table>
       </div>
     </div>
-  </div>
+</div>
   {{ $courses->links('laratrust::panel.pagination') }}
 @endsection
